aaffa98fb0d0448d03c902beedba468f21c5c27f
nahyun27 <ksknh7@naver.com>
2023-04-07 21:27:50 +0900

    [design] Add a homm screen #1
    - Implement a list of chat rooms
    - Arrange profile pictures by number of people

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,29 +1,128 @@
 import React, {useState, useEffect} from 'react';
-import {Text, View, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, Image} from 'react-native';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import styles from './style';
 import {ScrollView} from 'react-native-gesture-handler';
 import url from '../utils/backend';
 import axios from 'axios';
+import {widthPercentage} from '../Responsive';
 
 const Main = () => {
   const navigation = useNavigation();
+  const [rooms, setRooms] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    let tmp = [
+      {
+        title: '음성인식 회의1',
+        date: '2023.03.05',
+        members: ['김나현', '김지우', '김현기', '남윤재'],
+        duration: 35,
+      },
+      {title: '김나현', date: '2023.03.04', members: ['김나현'], duration: 52},
+      {
+        title: '음성인식 회의1',
+        date: '2023.03.02',
+        members: ['김지우', '김현기', '남윤재'],
+        duration: 63,
+      },
+      {
+        title: '금요일 닭갈비 밥약',
+        date: '2023.02.27',
+        members: ['김지우', '남윤재'],
+        duration: 13,
+      },
+    ];
+    setRooms(tmp);
+  }, []);
 
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <TouchableOpacity
-          onPress={() => {
-            return navigation.navigate('SpeakerList');
-          }}>
-          <View style={styles.button}>
-            <Text style={styles.btnText}>화자 목록</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <View>
+          <Text style={styles.title}>최근 대화 목록</Text>
+        </View>
+        {rooms.map((r, i) => (
+          <View key={i} style={styles.contentBox}>
+            <View style={styles.row}>
+              <View style={{marginRight: widthPercentage(10)}}>
+                {r.members.length === 1 ? (
+                  <Image
+                    source={require('../assets/images/profile1.png')}
+                    style={styles.profileImg1}
+                  />
+                ):
+                r.members.length === 2 ? (
+                  <View style={{width: widthPercentage(56), height: widthPercentage(56), flex: 1, justifyContent: 'space-between'}}>
+                    <Image
+                      source={require('../assets/images/profile3.png')}
+                      style={[styles.profileImg2, {
+                        marginBottom: widthPercentage(-8), marginRight: widthPercentage(-8)}]}
+                    />
+                    <Image
+                      source={require('../assets/images/profile4.png')}
+                      style={[styles.profileImg2, {justifyContent: 'flex-end', marginTop: widthPercentage(-8), marginLeft: widthPercentage(19)}]}
+                    />
+                </View>
+                  )
+                :
+                (<><View style={styles.center}>
+                  <Image
+                    source={require('../assets/images/profile1.png')}
+                    style={styles.profileImg4}
+                  />
+                  {r.members.length >= 4 &&
+                  <Image
+                    source={require('../assets/images/profile2.png')}
+                    style={styles.profileImg4}
+                  />}
+                </View>
+
+                <View style={styles.row}>
+                  <Image
+                    source={require('../assets/images/profile3.png')}
+                    style={styles.profileImg4}
+                  />
+                  <Image
+                    source={require('../assets/images/profile4.png')}
+                    style={styles.profileImg4}
+                  />
+                </View>
+                </>)}
+              </View>
+              <View style={styles.spaceB}>
+                <View>
+                  <Text style={styles.subtitle}>{r.title}</Text>
+                  <Text style={styles.descript}>
+                    {r.members[0]}님{r.members.length !== 1 && " 외 " + (r.members.length - 1) + "명"}과의 대화
+                  </Text>
+                  <Text style={styles.descript}>
+                    {r.date} 금요일 {r.duration}분
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  onPress={() => {
+                    return navigation.navigate('SpeakerList');
+                  }}>
+                  <Image
+                    source={require('../assets/images/more.png')}
+                    style={styles.icon}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
-        </TouchableOpacity>
-      </View>
-    </ScrollView>
+        ))}
+      </ScrollView>
+      <TouchableOpacity
+        onPress={() => {
+          return navigation.navigate('SpeakerList');
+        }}>
+        <View style={styles.button}>
+          <Text style={styles.btnText}>화자 목록</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 };
 
